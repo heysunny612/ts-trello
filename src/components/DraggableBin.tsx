fdd208617b1c92a38a80166b3e87a7d00241fec5
Sunny <heysunny612@gmail.com>
2023-06-15 02:34:38 +0900

    배포전 에러 수정

--- src/components/DraggableBin.tsx
+++ src/components/DraggableBin.tsx
@@ -1,6 +1,5 @@
 import { Droppable } from 'react-beautiful-dnd';
 import { BsTrash2Fill } from 'react-icons/bs';
-import { useRecoilValue } from 'recoil';
 
 export default function DroppableBin() {
   return (
